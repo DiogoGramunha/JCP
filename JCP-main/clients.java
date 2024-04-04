c555c720d997174baf1ff70752dc7e250819b6e3
DiogoGramunha <128035019+DiogoGramunha@users.noreply.github.com>
2024-04-04 18:50:10 +0100

    Create clients.java

---
+++ JCP-main/clients.java
@@ -0,0 +1,28 @@
+/**
+ * clientes
+ */
+public class clients {
+
+    private String name;
+    private int phone;
+    private String gmail;
+    
+
+    public clients(String name, int phone, String gmail){
+        this.name = name;
+        this.phone = phone;
+        this.gmail = gmail;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPhone(){
+        return phone;
+    }
+
+    public String getGmail(){
+        return gmail;
+    }
+}
